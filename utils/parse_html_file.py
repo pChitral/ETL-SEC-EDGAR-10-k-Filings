b5a53741c2a9cf4b06432e55e0a945e01cca3e35
pChitral <chitral.patil@vit.edu.in>
2023-09-22 04:00:30 -0400

    warning message handled

--- utils/parse_html_file.py
+++ utils/parse_html_file.py
@@ -4,7 +4,8 @@ from utils.find_general_section import find_general_section
 
 def parse_html_file(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
-        soup = BeautifulSoup(file.read(), "lxml")
+        soup = BeautifulSoup(file.read(), "lxml", features="xml")
+
     all_text = " ".join([tag.strip() for tag in soup.stripped_strings])
     risk_factors_section = find_general_section("Risk Factors", all_text)
     return {
