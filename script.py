0625526f474766625e5fc63d7cec8fc38197e1dd
pChitral <chitral.patil@vit.edu.in>
2023-09-22 02:34:43 -0400

    updated script

--- script.py
+++ script.py
@@ -9,68 +9,16 @@ from dotenv import load_dotenv
 
 from utils.get_ticker_10k_filings import get_ticker_10k_filings
 from utils.collect_ticker_files import collect_ticker_files
+from utils.new_10k_reports_to_supabase import new_10k_reports_to_supabase
+from utils.find_general_section import find_general_section
+from utils.delete_txt_files import delete_txt_files
+from utils.parse_html_file import parse_html_file
 
 # Supabase API keys
 load_dotenv()
 SUPABASE_URL = os.environ["SUPABASE_URL"]
 SUPABASE_KEY = os.environ["SUPABASE_KEY"]
 Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-
-# Function to insert parsed data into Supabase
-def new_10k_reports_to_supabase(all_parsed_data_list, Client):
-    try:
-        parsed_data_df = pd.DataFrame(all_parsed_data_list)
-        existing_reports_in_supabase = Client.table("reports_10k").select("*").execute()
-
-        if existing_reports_in_supabase.data:
-            existing_accession_numbers = [
-                record["accession_number"]
-                for record in existing_reports_in_supabase.data
-            ]
-        else:
-            existing_accession_numbers = []
-
-        filtered_reports_df = parsed_data_df[
-            ~parsed_data_df["accession_number"].isin(existing_accession_numbers)
-        ]
-        formatted_filtered_reports = filtered_reports_df.to_dict(orient="records")
-        data_reports = (
-            Client.table("reports_10k").insert(formatted_filtered_reports).execute()
-        )
-
-        assert len(data_reports.data) > 0, "No reports were embedded successfully."
-        return data_reports.data
-
-    except Exception as e:
-        return f'An error occurred during embedding: {e}'
-
-
-def find_general_section(title, text_content):
-    sections = re.split(r"Item\s+\d+", text_content)
-    for section in sections:
-        if re.search(re.escape(title), section, re.IGNORECASE):
-            return section.strip()
-    return None
-
-
-def delete_txt_files(files):
-    for file in files:
-        if file.endswith(".txt"):
-            os.remove(file)
-
-
-def parse_html_file(file_path):
-    with open(file_path, "r", encoding="utf-8") as file:
-        soup = BeautifulSoup(file.read(), "lxml")
-    all_text = " ".join([tag.strip() for tag in soup.stripped_strings])
-    risk_factors_section = find_general_section("Risk Factors", all_text)
-    parsed_data = {
-        "Risk Factors": risk_factors_section
-        if risk_factors_section
-        else "Section not found"
-    }
-    return parsed_data
 
 
 def process_ticker_10k_data(ticker):
