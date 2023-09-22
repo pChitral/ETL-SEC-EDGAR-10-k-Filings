ccf4c3740e2023c22d23e38bcb5b55752a019b53
pChitral <chitral.patil@vit.edu.in>
2023-09-22 03:32:58 -0400

    tickers all compiled

--- script.py
+++ script.py
@@ -76,10 +76,12 @@ def process_ticker_10k_data(ticker):
     return all_parsed_data
 
 
-# Example usage
-# Replace with your loop over tickers
+# Read the JSON file into a DataFrame
+df = pd.read_json("company_tickers.json", orient="index")
+
+# Process each ticker
 all_tickers_data = {}
-tickers = ["AAPL", "GOOG"]  # Add your list of tickers here
+tickers = df["ticker"].tolist()
 
 for ticker in tickers:
     all_tickers_data[ticker] = process_ticker_10k_data(ticker)
