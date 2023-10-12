045f3a3536a22e2cd4ac74a1674a6b4a304e88d6
pChitral <chitral.patil@vit.edu.in>
2023-10-12 12:23:50 -0500

    many funcs need to go in utils now

--- wc_scripy.py
+++ wc_scripy.py
@@ -102,9 +102,52 @@ def parse_html_file(file_path):
     }
 
 
-def new_10k_reports_to_supabase(parsed_data_list, client):
-    for data in parsed_data_list:
-        response = client.table("reports_10k").insert(data).execute()
+import pandas as pd
+
+
+def new_10k_reports_to_supabase(all_parsed_data_list, Client):
+    try:
+        # Convert the parsed data list to a DataFrame
+        parsed_data_df = pd.DataFrame(all_parsed_data_list)
+
+        # Fetch the existing reports from Supabase
+        existing_reports_in_supabase = Client.table("reports_10k").select("*").execute()
+
+        # Check if data exists and get the accession numbers
+        if existing_reports_in_supabase.data:
+            existing_accession_numbers = [
+                record["accession_number"]
+                for record in existing_reports_in_supabase.data
+            ]
+        else:
+            existing_accession_numbers = []
+
+        # Filter out the reports that are already in the database
+        filtered_reports_df = parsed_data_df[
+            ~parsed_data_df["accession_number"].isin(existing_accession_numbers)
+        ]
+        formatted_filtered_reports = filtered_reports_df.to_dict(orient="records")
+
+        # Insert the new data into the reports_10k table
+        if formatted_filtered_reports:
+            data_reports = (
+                Client.table("reports_10k").insert(formatted_filtered_reports).execute()
+            )
+
+            # Handle any errors during the insertion process
+            if data_reports.error:
+                print(f"Supabase Error: {data_reports.error}")
+
+            assert len(data_reports.data) > 0, "No reports were embedded successfully."
+            return data_reports.data
+
+        else:
+            print("No new reports to add.")
+            return []
+
+    except Exception as e:
+        print(f"An error occurred during embedding: {e}")
+        return []
 
 
 def process_ticker_10k_data(ticker):
