65dd487a96804aba89987679e42d87f6c9436b04
pChitral <chitral.patil@vit.edu.in>
2023-10-12 12:19:20 -0500

    compiling regex on top only once

--- wc_scripy.py
+++ wc_scripy.py
@@ -17,6 +17,9 @@ SUPABASE_URL = os.environ["SUPABASE_URL"]
 SUPABASE_KEY = os.environ["SUPABASE_KEY"]
 Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
+ITEM_7_PATTERN = re.compile(r"item\s*7\.")
+ITEM_8_PATTERN = re.compile(r"item\s*8\.")
+
 
 def extract_mda_section(file_path: str) -> str:
     """
@@ -27,7 +30,7 @@ def extract_mda_section(file_path: str) -> str:
     - file_path (str): Path to the HTML file of the 10-K report.
 
     Returns:
-    - str: Text of the MDA section.
+    - str: Cleaned text of the MDA section.
     """
     # Load and parse the HTML file content
     with open(file_path, "r", encoding="utf-8") as file:
@@ -37,12 +40,12 @@ def extract_mda_section(file_path: str) -> str:
     parsed_text = soup.get_text()
     lower_parsed_text = parsed_text.lower()
 
-    # Using a regex pattern to find all occurrences of "item 7." and "item 8."
+    # Using the precompiled regex patterns to find all occurrences of "item 7." and "item 8."
     item_7_matches = [
-        match.start() for match in re.finditer(r"item\s*7\.", lower_parsed_text)
+        match.start() for match in ITEM_7_PATTERN.finditer(lower_parsed_text)
     ]
     item_8_matches = [
-        match.start() for match in re.finditer(r"item\s*8\.", lower_parsed_text)
+        match.start() for match in ITEM_8_PATTERN.finditer(lower_parsed_text)
     ]
 
     # If we don't have any occurrence of "item 7." or "item 8.", return section not found
@@ -57,6 +60,10 @@ def extract_mda_section(file_path: str) -> str:
 
     # Extract content between the chosen "item 7." occurrence and the chosen "item 8." occurrence
     section_content = parsed_text[start_idx:end_idx].strip()
+
+    # Cleanup: Remove extra whitespace, newlines, and HTML entities
+    section_content = " ".join(section_content.split())
+    section_content = BeautifulSoup(section_content, "html.parser").get_text()
 
     return section_content
 
