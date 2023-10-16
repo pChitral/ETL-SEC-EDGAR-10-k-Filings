93804a4e6c6ff2ac756cd52996f25cbe1800ffdb
pChitral <chitral.patil@vit.edu.in>
2023-10-16 12:29:02 -0500

    updating counter

--- wc_scripy.py
+++ wc_scripy.py
@@ -10,6 +10,8 @@ from utils.collect_ticker_files import collect_ticker_files
 from utils.delete_txt_files import delete_txt_files
 from shutil import rmtree
 import re
+from collections import Counter
+import json
 
 # Supabase API keys
 load_dotenv()
@@ -78,17 +80,35 @@ def find_general_section(section_title, text):
     return text[start:end].strip()
 
 
-# Read the words from the provided file
+# Load the JSON file
 with open("words_fraud_constraints.json", "r") as file:
-    target_words = json.load(file)
+    data = json.load(file)
+# Combine the fraud and constraints words into a single list
+target_words = set(data["fraud"] + data["constraints"])
 
 
 def get_word_frequencies(text):
+    """
+    Get the frequency of target words related to "fraud" and "constraints" in a given text.
+
+    Parameters:
+    - text (str): The input text in which to search for the target words.
+
+    Returns:
+    - str: A JSON-formatted string containing the frequencies of the target words found in the text.
+    """
+
+    # Split the text into individual words
     words = text.split()
+
+    # Calculate word frequencies for the entire text
     frequency = Counter(words)
+
+    # Extract frequencies of the target words
     target_frequencies = {
         word: frequency[word] for word in target_words if word in frequency
     }
+
     return json.dumps(target_frequencies)
 
 
