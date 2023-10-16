49167b2dc12f5f9a5794694f44a083dedcd682e7
pChitral <chitral.patil@vit.edu.in>
2023-10-16 14:38:33 -0500

    running on ccr

--- wc_scripy.py
+++ wc_scripy.py
@@ -7,9 +7,6 @@ df = pd.read_json("company_tickers.json", orient="index")
 all_tickers_data = {}
 tickers = df["ticker"].tolist()
 
-count = 0
+
 for ticker in tickers:
     all_tickers_data[ticker] = process_ticker_10k_data(ticker)
-    count += 1
-    if count > 100:
-        break
