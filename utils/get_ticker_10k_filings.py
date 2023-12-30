fd2f6ba6300e133fb8813b7991daac67206c5e23
pChitral <chitral.patil@vit.edu.in>
2023-12-30 02:04:27 -0500

    Implemented Retry Logic

--- utils/get_ticker_10k_filings.py
+++ utils/get_ticker_10k_filings.py
@@ -1,35 +1,42 @@
 import logging
+import time
 from sec_edgar_downloader import Downloader
 
 
-def get_ticker_10k_filings(ticker):
+def get_ticker_10k_filings(ticker, max_retries=5, backoff_factor=1.0):
     """
-    Downloads all the 10-K filings for a given ticker from the SEC Edgar website.
+    Downloads all the 10-K filings for a given ticker from the SEC Edgar website with retry logic.
 
     Parameters:
         ticker (str): The ticker symbol of the company whose 10-K filings are to be downloaded.
+        max_retries (int): Maximum number of retry attempts.
+        backoff_factor (float): Factor to determine the wait time between retries.
 
     Returns:
-        None
-
-    Raises:
-        Exception: If any error occurs during the file download process, it will be logged.
+        bool: True if the download is successful, False otherwise.
 
     Example:
         get_ticker_10k_filings("MSFT")
     """
+
     # Create a downloader instance with the "data" folder as the destination
     dl = Downloader("SUNY_Buffalo", "hello@buffalo.edu", "data")
 
-    try:
-        # Get all 10-K filings for the specified ticker
-        # Use tqdm to add the progress bar
-        dl.get("10-K", ticker, download_details=True)
-    except Exception as e:
-        # Log the error message along with the ticker symbol
-        logging.error(
-            f"Error occurred while downloading 10-K filings for {ticker}: {e}"
-        )
+    for attempt in range(max_retries):
+        try:
+            # Get all 10-K filings for the specified ticker
+            dl.get("10-K", ticker, download_details=True)
+            return True  # Successful download
+        except Exception as e:
+            logging.error(f"Attempt {attempt + 1} failed for {ticker}: {e}")
+
+            # Calculate sleep time using exponential backoff strategy
+            sleep_time = backoff_factor * (2**attempt)
+            time.sleep(sleep_time)
+
+    # If all attempts fail, log and return False
+    logging.error(f"All attempts to download 10-K filings for {ticker} have failed.")
+    return False
 
 
 # Example usage:
@@ -42,4 +49,7 @@ if __name__ == "__main__":
     )
 
     ticker_name = "MSFT"  # Replace this with the desired ticker name
-    get_ticker_10k_filings(ticker_name)
+    success = get_ticker_10k_filings(ticker_name)
+
+    if not success:
+        logging.error(f"Failed to download filings for {ticker_name}")
