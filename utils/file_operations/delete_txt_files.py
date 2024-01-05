6643de10dfc66eca32a7636dd15f153caf5b0406
pChitral <chitral.patil@vit.edu.in>
2024-01-05 13:35:27 -0500

    Refactor delete_txt_files function to use type hints and improve readability

--- utils/file_operations/delete_txt_files.py
+++ utils/file_operations/delete_txt_files.py
@@ -1,7 +1,17 @@
 import os
+from typing import List
 
 
-def delete_txt_files(files):
-    for file in files:
-        if file.endswith(".txt"):
-            os.remove(file)
+def delete_txt_files(files: List[str]) -> None:
+    """
+    Deletes all files with a '.txt' extension from a list of file names.
+
+    Parameters:
+        files (List[str]): A list of file names (strings) to be checked and deleted if they end with '.txt'.
+
+    Returns:
+        None: This function does not return anything. It performs a deletion operation on files.
+    """
+    txt_files = [file for file in files if file.endswith(".txt")]
+    for file in txt_files:
+        os.remove(file)
