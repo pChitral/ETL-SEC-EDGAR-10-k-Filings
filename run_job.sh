54ac85371107f4f01d78c01095bbfd85604152ab
pChitral <chitral.patil@vit.edu.in>
2024-01-25 02:40:59 -0500

    Update job configuration in run_job.sh

--- run_job.sh
+++ run_job.sh
@@ -2,17 +2,19 @@
 set -e  # Exit immediately if a command exits with a non-zero status.
 
 #SBATCH --time=24:00:00              # Specify the maximum runtime (here, 24 hours)
-#SBATCH --nodes=1                   # Specify the number of nodes (here, 1)
-#SBATCH --ntasks-per-node=1        # Specify the number of tasks per node (here, 1)
-#SBATCH --mem=50000                 # Specify the memory requirement in MB (50 GB = 50000 MB)
+#SBATCH --nodes=1                    # Specify the number of nodes (here, 1)
+#SBATCH --ntasks-per-node=16          # Specify the number of tasks per node (here, 16)
+#SBATCH --mem=75000                  # Specify the memory requirement in MB (75 GB = 75000 MB)
 #SBATCH --job-name="ETL_SEC_EDGAR_10k_filings"   # Specify the job name
 #SBATCH --output=ETL_SEC_EDGAR_10k_filings.out   # Specify the output file
 #SBATCH --error=ETL_SEC_EDGAR_10k_filings.err    # Specify the error file
 #SBATCH --mail-user=chitralp@buffalo.edu         # Specify your email address
 #SBATCH --mail-type=end
 #SBATCH --partition=general-compute              # Specify the partition (general-compute)
-#SBATCH --qos=general-compute                    # Specify the quality of service (general-compute)
+#SBATCH --qos=general-c+                         # Updated QoS setting
 #SBATCH --cluster=ub-hpc                         # Specify the cluster name (ub-hpc)
+
+# [Rest of your script]
 
 # Navigate to the directory where you want to run the Python script
 cd /user/chitralp/ETL-10-k-Filings
