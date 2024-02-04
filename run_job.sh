dd74bed391c0aa6404cbd53b4739e5a020d5f2bc
pChitral <chitral.patil@vit.edu.in>
2024-02-04 15:27:21 -0500

    Update run_job.sh script to activate Python virtual environment and stream output to a file

--- run_job.sh
+++ run_job.sh
@@ -11,11 +11,10 @@
 #SBATCH --partition=general-compute
 #SBATCH --qos=general-compute
 #SBATCH --cluster=ub-hpc
-# Navigate to the directory where you want to run the Python script
+
 cd /projects/academic/haimonti/ETL_10k_filings/word_count/ETL-SEC-EDGAR-10-k-Filings
 
-# Activate your Python virtual environment
 source venv/bin/activate
 
-# Run your Python script and use 'tee' for streaming the output
+
 python3 scrape_entire_text_mda.py | tee ETL_SEC_EDGAR_10k_filings_stream.out
